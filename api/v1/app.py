92295decdbac33c3ade3992ec0009b3e4dd14476
Benjamin Atsu <Bredrenbonish@gmail.com>
2024-05-29 15:25:22 +0000

    Add CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,31 @@ from flasgger.utils import swag_from
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+app.config['SWAGGER'] = {
+    'title': 'LEARNXCEL Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 if __name__ == "__main__":
     """ Main Function """
